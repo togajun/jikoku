8082ccb55128f6f749ee10bddb3f6427c9be349b
{togajun} <{togajune43@gmail.com}>
2024-09-09 14:16:55 +0900

    初めのコミット

---
+++ nakanohujimi.py
@@ -0,0 +1,32 @@
+import os
+import datetime
+import numpy as np
+import pandas as pd
+
+wd=datetime.datetime.today().weekday()
+
+def weekdays_diagram();
+    
+
+
+
+def saturdays_diagram():
+    
+
+
+
+
+def sundays_diagram():
+
+
+
+
+
+
+if wd == 5:
+    saturdays_diagram()
+elif wd == 6:  
+    sundays_diagram()
+else:
+    weekdays_diagram()
+    
